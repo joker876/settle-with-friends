b561cbc94ec14a6bd82a10f9ab2711adfde70418
joker876 <joker876x@gmail.com>
2025-10-11 17:25:46 +0200

    add page title handling to login page

--- client/src/app/features/login/views/login/login.view.ts
+++ client/src/app/features/login/views/login/login.view.ts
@@ -1,6 +1,7 @@
 import { Component, effect, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService, LogoutReason } from '@common/services/auth.service';
+import { TitleService } from '@common/services/title.service';
 import { LoginGoogleComponent } from '@features/login/components/login-google/login-google.component';
 
 @Component({
@@ -12,11 +13,30 @@ import { LoginGoogleComponent } from '@features/login/components/login-google/lo
 export class LoginView {
   private readonly _authService = inject(AuthService);
   private readonly _router = inject(Router);
+  private readonly _titleService = inject(TitleService);
 
   readonly logoutReason = this._authService.logoutReason;
   readonly LogoutReason = LogoutReason;
 
   constructor() {
+    effect(() => {
+      const logoutReason = this._authService.logoutReason();
+      let title = '';
+      switch (logoutReason) {
+        case LogoutReason.LoggedOut:
+          title = $localize`:@@login.page-title.logged-out:Wylogowane poprawnie`;
+          break;
+
+        case LogoutReason.SessionExpired:
+          title = $localize`:@@login.page-title.logged-out:Sesja logowania wygasła`;
+          break;
+
+        default:
+          title = $localize`:@@login.page-title.default:Logowanie`;
+          break;
+      }
+      this._titleService.currentBaseTitle.set(title);
+    });
     effect(() => {
       if (this._authService.isSafeToRedirect() && this._authService.isLoggedIn()) {
         this._router.navigateByUrl('/');
