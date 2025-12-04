02da3c0b14d1a3bbfbe3b5656689b0c310a0670f
joker876 <joker876x@gmail.com>
2025-12-04 17:10:22 +0100

    start implementing creating transactions

---
+++ client/src/app/features/reckoning/views/create-transaction/create-transaction.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-transaction',
+  imports: [],
+  templateUrl: './create-transaction.page.html',
+  styleUrl: './create-transaction.page.scss'
+})
+export class CreateTransactionPage {
+
+}
